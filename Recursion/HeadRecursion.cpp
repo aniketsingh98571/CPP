ceda5bfb9535a7042240d69e98e9a56e0606da41
aniketsingh98571 <thor98571@gmail.com>
2021-11-18 11:43:50 +0530

    Initial Commit

---
+++ Recursion/HeadRecursion.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+void fun(int n){
+    if(n>0){
+        
+        fun(n-1);
+        cout<<n<<" ";
+        
+    }
+}
+
+int main()
+{
+    
+    fun(3);
+    return 0;
+}
